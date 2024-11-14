aa9d89e56bd874e91651700b43f64b1a8572fbc5
preet006 <preetbhardwaj146@gmail.com>
2024-11-14 21:52:31 +0530

    chore: remove unsused code

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,27 +5,6 @@ export interface SidebarLink {
   route: string;
   label: string;
 }
-
-export interface Job {
-  id?: string;
-  employer_name?: string;
-  employer_logo?: string | undefined;
-  employer_website?: string;
-  job_employment_type?: string;
-  job_title?: string;
-  job_description?: string;
-  job_apply_link?: string;
-  job_city?: string;
-  job_state?: string;
-  job_country?: string;
-}
-
-export interface Country {
-  name: {
-    common: string;
-  };
-}
-
 export interface ParamsProps {
   params: { id: string };
 }
@@ -39,10 +18,3 @@ export interface URLProps {
   searchParams: { [key: string]: string | undefined };
 }
 
-export interface BadgeCounts {
-  GOLD: number;
-  SILVER: number;
-  BRONZE: number;
-}
-
-export type BadgeCriteriaType = keyof typeof BADGE_CRITERIA;
